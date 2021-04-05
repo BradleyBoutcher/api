0868023a7013d02f6f1977f35619af0e07797660
Bradley Boutcher <btboutcher@icloud.com>
2021-04-05 15:46:09 -0400

    Add converted openapi spec

--- convert.sh
+++ convert.sh
@@ -9,20 +9,27 @@
 cd "$(dirname "$0")"
 
 # Spec we're using as a base
-readonly spec="openapi-3.yml"
+readonly SPEC="openapi-3.yml"
 # File to parse
-readonly source="entries.txt"
+readonly ENTRIES="entries.txt"
+# File to store mappings of entries to their filepaths 
+readonly MAPPINGS="mappings.txt"
 # Top-level directory containing openapi files
-readonly top_level_dir="./openapi"
+readonly TOP_LEVEL_DIR="./test"
 # File containing uncategorized entries
-readonly shared_file="$top_level_dir/shared.yml"
+readonly SHARED_FILE="shared.yml"
 
-yq eval '(.components.schemas | keys | .[] )' "$spec" > "$source"    
+# # Cleanup
+rm -rf $TOP_LEVEL_DIR 2> /dev/null
+rm -rf $MAPPINGS 2> /dev/null
+rm -rf $ENTRIES 2> /dev/null
 
-# Cleanup
-rm -rf top_level_dir
+# Generate our list of entries
+yq eval '(.components.schemas | keys | .[] )' "$SPEC" > "$ENTRIES"    
 
-# Begin parsing
+touch $MAPPINGS
+
+# Begin creating our fragmented spec
 while read line; do 
     # Replace all periods in the line with spaces
     # convert to lowercase
@@ -33,7 +40,7 @@ while read line; do
     # Retrieve entry
     entry=$(line="$line" yq eval '.components.schemas.[env(line)]' openapi-3.yml)
     # Retrieve the name of our entry
-    entryname="${words[-1]}:"
+    entryname="${words[-1]}"
 
     # Remove the nth item (we don't need it anymore)
     words=${words[@]:0:${#words[@]}-1}
@@ -41,33 +48,96 @@ while read line; do
     # Get the length of our word array
     length=${#words[@]}
 
-    FILEPATH=""
+    filepath=""
     # If there's no words left, skip it
     if [[ $length -lt 1 ]]; then
         echo "Single-item entry, adding to shared file..."
-        FILEPATH="$shared_file"
+        filepath="$SHARED_FILE"
     elif [[ $length -eq 1 ]]; then 
     # If there's only one word left, make a file
-        FILEPATH="$top_level_dir/${words[0]}"
+        filepath="${words[0]}"
     else 
     # Convert all words to a single string
-        FILEPATH="$top_level_dir/${words[@]// //}.yml"
+        filepath="${words[@]// //}.yml"
     fi
     
+    # Record the entry and the associated filepath
+    echo "$entryname $line $filepath" >> $MAPPINGS
+
+    # Lowercase expansion for ZSH shells
+    filepath="$TOP_LEVEL_DIR/${filepath:l}"
+
     # Create our filepath, if needed
-    if [ ! -f "$FILEPATH" ]; then
-        mkdir -p "$(dirname "$FILEPATH")" && touch "$FILEPATH"
-        echo "components:" >> "$FILEPATH"
-        echo "  schemas:" >> "$FILEPATH"
-        echo "$FILEPATH created!"
+    if [ ! -f "$filepath" ]; then
+        mkdir -p "$(dirname "$filepath")" && touch "$filepath"
+        echo "components:" >> "$filepath"
+        echo "  schemas:" >> "$filepath"
+        echo "$filepath created!"
     else 
         echo "File exists, skipping..."
     fi
 
     # Add our new shortened entry name
-    echo $entryname | awk '{ print "    " $0 }' >> $FILEPATH
+    echo "$entryname:" | awk '{ print "    " $0 }' >> $filepath
     # Add the entire entry 
-    echo $entry | awk '{ print "      " $0 }' >> $FILEPATH
+    echo "$entry" | awk '{ print "      " $0 }' >> $filepath
+    # Add a space between entries
+    echo >> $filepath
 
     echo
-done < $source
+done < $ENTRIES
+
+declare -a files 
+# files=($(find "$TOP_LEVEL_DIR" -type f))
+files=("./test/openapi.yml")
+
+# Recursively "fix" refs
+while read line; do
+    words=(`echo ${line}`);
+    
+    entryname=${words[1]}
+    oldref=${words[2]}   
+    filepath="$TOP_LEVEL_DIR/${words[3]:l}"
+    # Get absolute path for source file
+    filepath=$(realpath $filepath)
+
+    echo "
+    ============
+    Now replacing incorrect references for $oldref
+    Location for entry: $filepath
+    ============
+    "
+
+    # We first need to create a period-escaped string
+    # representation of the entry we want to replace
+    # First, escape periods
+    target="$(echo $oldref | sed 's/[.]/\\./g')"
+    # Second, escape left brackets (some entries end with [])
+    target="$(echo $target | sed 's/\[/\\[/g')"
+    # Finally, escape right brackets
+    target="$(echo $target | sed 's/\]/\\]/g')"
+
+    # For every new file, check if there's a reference to the entry
+    # If we find it, replace it with its new path.
+    for file in ${files[@]}; do
+        # Get absolute path for target file
+        dir=$(realpath $file)
+        echo "Replacing in $dir..."
+        # Location of file relative to current file
+        relative_path=$(realpath --relative-to="$dir" "$filepath")
+
+        # Fix off-by-one directory 
+        if [[ "$relative_path" == "." ]]; then
+            relative_path=""
+        else
+            relative_path=$(echo "$relative_path" | sed "s~^../~~g")
+        fi
+
+        echo "Entry location relative to current file: $relative_path"
+        # New location
+        new_ref="'$relative_path#/components/schemas/$entryname'"
+        # Replace replace replace
+        sed -i '' "s~'.*$target'~$new_ref~g" $file
+    done
+
+done < $MAPPINGS
